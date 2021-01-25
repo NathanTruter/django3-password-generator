22e3ce46bf936d59415cad4b688f5275eb6bb0e2
Nathan Truter <enty15@hotmail.com>
2021-01-25 15:30:07 +0800

    I made some tweeks

--- generator/views.py
+++ generator/views.py
@@ -14,7 +14,7 @@ def password(request):
     if request.GET.get('uppercase'):
         characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
     if request.GET.get('special'):
-        characters.extend(list('!@#$%&*'))
+        characters.extend(list('!@#$%&*+'))
     if request.GET.get('numbers'):
         characters.extend(list('123456789'))
 
